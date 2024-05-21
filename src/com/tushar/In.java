c876297f6bbc05a7e54715f2e08a5f395aeadf16
itushar2002 <itusharjha2002@gmail.com>
2024-05-21 18:00:03 +0530

    Java_Input

---
+++ src/com/tushar/In.java
@@ -0,0 +1,9 @@
+package com.tushar;
+import java.util.Scanner;
+public class In {
+    public static  void main(String[] args) {
+        System.out.println("How are you?");
+       Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
+    }
+}
