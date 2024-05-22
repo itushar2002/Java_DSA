b10ef8745232cc19c0a026ad5752f701f792300a
itushar2002 <itusharjha2002@gmail.com>
2024-05-22 12:05:59 +0530

    While loop in java

---
+++ src/com/tushar/LoopWhile.java
@@ -0,0 +1,19 @@
+package com.tushar;
+
+// while loop Syntax
+//while (condition) {
+//    // body
+//     //   iteration
+//        }
+
+
+
+public interface LoopWhile {
+    public static void main(String[] args) {
+        int count = 1;
+        while(count != 20){ // condition ( boolean ) // jab tak (cond) tab tak
+            System.out.println(count); // (body/task)  ye karo
+            count++; // (iteration count ) // ++ ya --
+        }
+    }
+}
